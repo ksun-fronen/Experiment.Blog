6cd93e2103297d9aad90281cdd22d6bb48bb62ac
曾家淳 <szjyp226@163.com>
2023-12-01 18:04:05 +0800

    add eslint add i18n add lint-staged add husky

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import store from './store'
+import installI18n from './lang/index'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+installI18n(app)
+app.use(store).mount('#app')

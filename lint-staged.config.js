a99b6fcd2f1e94d8eb39b202815680dcaa239832
曾家淳 <szjyp226@163.com>
2023-12-05 09:50:46 +0800

    upgrade eslint config and test

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,17 @@
-module.exports = {
-  '*.{js,jsx,vue}': 'vue-cli-service lint'
-}
+import micromatch from "micromatch";
+
+export default {
+  "*.{js,jsx,vue,ts,tsx}": (files) => {
+    files = micromatch.not(files, "**/*.config.js");
+    console.log("files", files);
+    return `eslint --fix ${files.join(" --fix ")}`;
+  },
+  /*
+  "ignore": [
+    "node_modules",
+    "dist",
+    "package-lock.json",
+    ".config.js",
+  ]
+  */
+};
---
+++ src/components/TestComponent.vue
@@ -0,0 +1,13 @@
+<script setup>
+console.log("123");
+
+alert("ww test");
+</script>
+
+<template>
+  <div>123</div>
+</template>
+
+<style scoped>
+
+</style>

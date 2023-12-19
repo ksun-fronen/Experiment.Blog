5785cb39b3f62109ddc66e72c6b9d4b5172b1cd1
曾家淳 <szjyp226@163.com>
2023-12-19 18:37:00 +0800

    upgrade .env

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,41 @@
 import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import siteConfig from "./src/config/site.json";
-
-const transformHtmlPlugin = data => ({
-  name: "transform-html",
-  transformIndexHtml: {
-    enforce: "pre",
-    transform(html) {
-      return html.replace(
-        /#{\s*(\w+)\s*}/gi,
-        (match, p1) => data[p1] || ""
-      );
-    }
-  }
-});
+import vue            from "@vitejs/plugin-vue";
+// import siteConfig     from "./src/config/site.json";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    transformHtmlPlugin(siteConfig.IndexTemplateVariable),
-  ],
+export default defineConfig(() => {
+  // todo: 如果是本地部署 - 则采用sourcemap的方式, 如果是build部署, 则忽略sourcemap
+  return {
+    envDir: "./config",
+    plugins: [
+      vue(),
+      /*transformHtmlPlugin(
+        Object.assign(
+          {},
+          siteConfig.IndexTemplateVariable,
+          siteConfig.IndexTemplateVariable[config.mode] || {}
+        )
+      ),*/
+    ],
+  };
 });
+
+/*function transformHtmlPlugin(data) {
+  return {
+    name: "transform-html",
+    transformIndexHtml: {
+      enforce: "pre",
+      transform(html) {
+        return html.replace(
+          /#{\s*([\w.]+)\s*}/gi,
+          (match, p1) => {
+            if (p1) {
+              return p1.split(".").reduce((data, prop) => data[prop] || "", data);
+            }
+            return "";
+          }
+        );
+      }
+    }
+  };
+}*/

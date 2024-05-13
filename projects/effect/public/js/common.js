88c8ef134ab21fe3ff5ab48e7f4f4b35100e04f5
曾家淳 <szjyp226@163.com>
2024-05-13 16:04:20 +0800

    添加了新的启动动画样式和脚本
    在HTML文件中，我们更新了加载动画的SVG和相关的样式，在初始化.scss文件中，添加了`.overflow-hidden`类，在新的animation.scss文件中，定义了加载动画的CSS样式和动画，同时，我们在common.js脚本中添加了动画结束后的处理函数。所有的这些修改都是用来提高用户体验，增加网站的生动性和动态效果。

--- projects/effect/public/js/common.js
+++ projects/effect/public/js/common.js
@@ -23,6 +23,7 @@ window.setLoadingState = (function () {
     if (resultPercent >= 100) {
       // document.getElementById("LoadingContainerRef").classList.add("off");
       clearInterval(int);
+      onFinishScriptList();
       window.setLoadingState = () => "";
     }
   };
@@ -41,4 +42,31 @@ window.setLoadingState = (function () {
 
 function onLoad() {
   window.setLoadingState(20);
+}
+
+// script加载全部完成时候
+function onFinishScriptList() {
+  const mask = document.getElementById("CircleLoopMask");
+  const mask2 = document.getElementById("CircleLoopMask2");
+  const mask3 = document.getElementById("CircleLoopMask3");
+  const maskContainer = mask.children[0];
+  const mask2Container = mask2.children[0];
+  const mask3Container = mask3.children[0];
+  // maskContainer容器的动画结束时
+  maskContainer.addEventListener("animationend", function _AnimationEnd(e) {
+    if (e.currentTarget === maskContainer) {
+      mask2Container.addEventListener("animationend", _AnimationEnd);
+      mask2Container.classList.add("start");
+      maskContainer.removeEventListener("animationend", _AnimationEnd);
+    } else if (e.currentTarget === mask2Container) {
+      mask3Container.addEventListener("animationend", _AnimationEnd);
+      mask3Container.classList.add("start");
+      mask2Container.removeEventListener("animationend", _AnimationEnd);
+    } else if (e.currentTarget === mask3Container) {
+      document.getElementById("LoadingSVG").style.display = "none";
+      document.body.classList.remove("overflow-hidden");
+      mask3Container.removeEventListener("animationend", _AnimationEnd);
+    }
+  });
+  maskContainer.classList.add("start");
 }

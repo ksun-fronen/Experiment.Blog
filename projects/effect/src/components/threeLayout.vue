0fbec15b4f2a08b388103bbdb536ecb28c20e96a
曾家淳 <szjyp226@163.com>
2024-06-21 11:40:37 +0800

    [开发记录] 设置摄像头

--- projects/effect/src/components/threeLayout.vue
+++ projects/effect/src/components/threeLayout.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onBeforeMount, onMounted, Ref, ref } from "vue";
+import { onBeforeMount, onMounted, onUnmounted, Ref, ref } from "vue";
 import * as THREE from "three";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
@@ -11,41 +11,55 @@ const loader = new GLTFLoader();
 // const girlModelAsset = new URL("/assets/sketch-fab_common-models/just_a_girl/scene.gltf", import.meta.url);
 // console.log("girlModelAsset", girlModelAsset);
 
+let isDestroyed = false;
+
 onMounted(() => {
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
   const renderer = new THREE.WebGLRenderer();
 
+  camera.position.set(0, 120, 120);
   // 官方例子导入
   renderer.outputColorSpace = THREE.SRGBColorSpace;
 
+  // 设置渲染容器的实际大小
   renderer.setSize(window.innerWidth, window.innerHeight);
   loader.load("/assets/sketch-fab_common-models/just_a_girl/scene.gltf", (gltf) => {
     console.log("gltf", gltf);
     scene.add(gltf.scene);
-
-    animate();
-  }, void 0, (error) => {
+    // gltf.scene.rotateX(320);
+    render();
+  }, undefined, (error) => {
     console.error(error);
   });
   if (canvasLayoutRef.value) {
     canvasLayoutRef.value.appendChild(renderer.domElement);
   }
 
+  // 摄像头控制
   const controls = new OrbitControls(camera, renderer.domElement);
-  controls.addEventListener("change", animate); // use if there is no animation loop
-  controls.minDistance = 400;
+  controls.addEventListener("change", render); // use if there is no animation loop
+  controls.minDistance = 100;
   controls.maxDistance = 1000;
-  controls.target.set(10, 90, -16);
+  controls.target.set(0, 30, 0);
   controls.update();
 
-  window.addEventListener("resize", animate);
+  window.addEventListener("resize", render);
 
-  function animate() {
-    requestAnimationFrame(animate);
+  function render() {
     renderer.render(scene, camera);
   }
+
+  function animate() {
+    if (isDestroyed) return;
+    requestAnimationFrame(animate);
+    render();
+  }
   animate();
+});
+
+onUnmounted(() => {
+  isDestroyed = true;
 });
 </script>
 
